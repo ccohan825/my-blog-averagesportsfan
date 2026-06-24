a64f2b12ccd611f276ab92fbe0e4d37ca59a30bd
ccohan825 <ccohan825@gmail.com>
2026-06-24 10:12:40 -0700

    Fix posts file and blog order

--- data/posts.js
+++ data/posts.js
@@ -1,13 +1,5 @@
 const posts = [
   {
-    title: "The Supplemental Draft: What It Is and How It Works",
-    description: "Why the NFL created the Supplemental Draft, how the process works, and the story of Al Hunter.",
-    category: "NFL",
-    date: "2026-06-22",
-    displayDate: "June 22, 2026",
-    url: "blogs/the-supplemental-draft-what-it-is-and-how-it-works.html"
-  },
-  {
     title: "The NFL Has a Huge Gambling Problem",
     description: "Since 2019 over 10 players in the league have been suspended for gambling violations and the league denying Brendan Sorsby to the supplemental draft is the league's newest hypocritical moves.",
     category: "NFL",
@@ -15,3 +7,12 @@ const posts = [
     displayDate: "June 24, 2026",
     url: "blogs/the-nfl-has-a-huge-gambling-problem.html"
   },
+  {
+    title: "The Supplemental Draft: What It Is and How It Works",
+    description: "Why the NFL created the Supplemental Draft, how the process works, and the story of Al Hunter.",
+    category: "NFL",
+    date: "2026-06-22",
+    displayDate: "June 22, 2026",
+    url: "blogs/the-supplemental-draft-what-it-is-and-how-it-works.html"
+  }
+];
